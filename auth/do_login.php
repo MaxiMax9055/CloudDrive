ed2b2b65d7d9717697752fccd8093cd615dcf9b1
MaxiMax9055 <151159289+MaxiMax9055@users.noreply.github.com>
2024-11-08 00:58:07 +0300

    Add files via upload

---
+++ auth/do_login.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__.'/boot.php';
+
+// Проверяем наличие пользователя с указанным юзернеймом
+$stmt = pdo()->prepare("SELECT * FROM `users` WHERE `username` = :username");
+$stmt->execute(['username' => $_POST['username']]);
+if (!$stmt->rowCount()) {
+    flash('Пользователь с такими данными не зарегистрирован');
+    header('Location: ../index.php');
+    die;
+}
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// Проверяем пароль
+if (password_verify($_POST['password'], $user['password'])) {
+    // Проверяем, не нужно ли использовать более новый алгоритм
+    // или другую алгоритмическую стоимость
+    // Например, если вы поменяете опции хеширования
+    if (password_needs_rehash($user['password'], PASSWORD_DEFAULT)) {
+        $newHash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $stmt = pdo()->prepare('UPDATE `users` SET `password` = :password WHERE `username` = :username');
+        $stmt->execute([
+            'username' => $_POST['username'],
+            'password' => $newHash,
+        ]);
+    }
+    $_SESSION['user_id'] = $user['id'];
+    setcookie("user_id", $_SESSION['user_id']);
+    header('Location: ../index.php');
+    die;
+}
+
+flash('Пароль неверен');
+header('Location: ../index.php');
